e3185e47da02793369effa103c08436dfb7e8834
Alexander_Esin <alexavalon@yandex.ru>
2023-10-27 20:20:47 +0700

    add codes in class Main for Test

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,25 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Алексей", "Ватин", (byte) 25);
+        userService.saveUser("Дмитрий", "Иванов", (byte) 34);
+        userService.saveUser("Иван", "Соколов", (byte) 27);
+        userService.saveUser("Мария", "Бунина", (byte) 24);
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
