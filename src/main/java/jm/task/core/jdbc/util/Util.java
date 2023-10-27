7ee7b127a7a0f3a48bddac7ba708ef6975957b92
Alexander_Esin <alexavalon@yandex.ru>
2023-10-27 13:37:27 +0700

    Add connection in class Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private final String HOST = "jdbc:mysql://localhost:3306/prepro2";
+    private final String USERNAME = "root";
+    private final String PASSWORD = "Grind02Iop4";
+
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(HOST, USERNAME, PASSWORD);
+            System.out.println("Connection OK");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Connection ERROR");
+        }
+        return connection;
+    }
 }
